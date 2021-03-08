c2dd29fbaf8aa36ad503dd081d85ea8c992c07a3
chaticker <cseun999@naver.com>
2021-03-08 13:24:11 +0900

    입력 정보에 대한 보안

--- main.js
+++ main.js
@@ -2,38 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-//refactoring
-//객체 선언
-var template = {
-  //프로퍼티 선언
-  html:function (title, list, body, control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list:function (filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list+'</ul>';
-    return list;
-  }
-}
+var template = require('./lib/template.js');
+var path = require('path');
  
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -45,7 +15,7 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = template.list(filelist);
-          var html = template.html(title, list,
+          var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>`
           );
@@ -54,17 +24,19 @@ var app = http.createServer(function(request,response){
         });
       } else {
         fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var filteredId = path.parse(queryData.id).base;
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = template.list(filelist);
-            var html = template.html(title, list,
+            var html = template.HTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> 
-               <a href="/update?id=${title}">update</a>
-               <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <input type="submit" value="delete">
-               </form>`
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
             );
             response.writeHead(200);
             response.end(html);
@@ -75,7 +47,7 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
-        var html = template.html(title, list, `
+        var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -103,16 +75,17 @@ var app = http.createServer(function(request,response){
             response.end();
           })
       });
-    } else if(pathname ==='/update') {
+    } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
-          var html = template.html(title, list,
+          var html = template.HTML(title, list,
             `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p> 
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
               <p>
                 <textarea name="description" placeholder="description">${description}</textarea>
               </p>
@@ -142,7 +115,7 @@ var app = http.createServer(function(request,response){
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
             })
-          })
+          });
       });
     } else if(pathname === '/delete_process'){
       var body = '';
@@ -152,7 +125,8 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
-          fs.unlink(`data/${id}`, function(error){
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
             response.writeHead(302, {Location: `/`});
             response.end();
           })
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports = {
+    id:'chaticker',
+    password:'12345'
+}
